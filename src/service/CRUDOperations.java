9544f028fe2ea222a73568fae407247b229e0b04
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-18 23:35:07 +0530

    Add files via upload

--- src/service/CRUDOperations.java
+++ src/service/CRUDOperations.java
@@ -6,24 +6,48 @@ import java.util.List;
 
 import model.Blog;
 
-public class CRUDOperations {
-	static List<Blog> list=new ArrayList<Blog>(); // Store the blog in the list
+static List<Blog> list=new ArrayList<Blog>(); // Store the blog in the list
+
+public List<Blog> addBlog(Blog blog) throws IOException {
+	list.add(blog);
+	return list;
+}
+
+public List<Blog> viewBlog(Blog blog) throws IOException {
+	for(int i=0; i<list.size();i++) {
+		System.out.println(list.get(i).getBlogTitle()+" "+list.get(i).getBlogDescription());
+	}
+	return list;
+}
+
+public List<Blog> createBlog(Blog blog) throws IOException {
+	list.add(blog);
+	return list;
+}
+
+public List<String> updateBlog(Blog blog) throws IOException{
+	List<String> newlist = new ArrayList<String>();
 	
-	public List<Blog> createBlog(Blog blog) throws IOException {
-		list.add(blog);
-		return list;
+	list.sort((Blog s1, Blog s2) -> s1.getBlogTitle().compareTo(s2.getBlogTitle()));
+	for(int i=0; i<list.size();i++) {
+		newlist.add(list.get(i).getBlogTitle());
 	}
+return newlist;
+}
+
+public List<LocalDate> updateBlogdate(Blog blog) throws IOException{
+	List<LocalDate> newlist = new ArrayList<LocalDate>();
 	
-	public List<Blog> viewBlog(Blog blog) throws IOException{
-		return null;
-		
+	list.sort((Blog s1, Blog s2) -> s1.getDate().compareTo(s2.getDate()));
+	for(int i=0; i<list.size();i++) {
+		newlist.add(list.get(i).getDate());
 	}
-	public List<Blog> updateBlog(Blog blog) throws IOException{
-		return null;
+return newlist;
+}
+
+public List<Blog> deleteBlog(Blog blog) throws IOException{
+	list.remove(blog);
+	return list;
 	
-	}
-	public List<Blog> deleteBlog(Blog blog) throws IOException{
-		return null;
-		
-	}
+}
 }
